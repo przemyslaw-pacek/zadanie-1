3eb74bce6b6b272b7e419fe99a113c42ec3fc75f
Przemyslaw Pacek <ppacek@op.pl>
2025-11-04 16:20:22 +0100

    shuffleWord logic moved to a separate file

---
+++ src/Form/shuffleWord.js
@@ -0,0 +1,14 @@
+export const shuffleWord = (word) => {
+  if (word.length < 4) return word;
+
+  const first = word[0];
+  const last = word[word.length - 1];
+  const middle = word.slice(1, -1).split("");
+
+  for (let i = middle.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [middle[i], middle[j]] = [middle[j], middle[i]];
+  }
+
+  return `${first}${middle.join("")}${last}`;
+};
